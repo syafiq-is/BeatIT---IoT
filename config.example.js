4362b725d84d27eaf3dff2dacb5a88b581b8ac03
syafiq-is <syafiqilham.sho@gmail.com>
2025-07-12 00:06:16 +0700

    initial commit

---
+++ config.example.js
@@ -0,0 +1,21 @@
+// firebaseConfig.js
+export const firebaseNotificationConfig = {
+  apiKey: "[YOUR_SECRET]",
+  authDomain: "[YOUR_SECRET].firebaseapp.com",
+  projectId: "[YOUR_SECRET]",
+  storageBucket: "[YOUR_SECRET].appspot.com",
+  messagingSenderId: "[YOUR_SECRET]",
+  appId: "[YOUR_SECRET]",
+};
+
+// Your Firebase config
+export const firebaseConfig = {
+  apiKey: "[YOUR_SECRET]",
+  authDomain: "[YOUR_SECRET].firebaseapp.com",
+  databaseURL:
+    "https://[YOUR_SECRET]-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "[YOUR_SECRET]",
+  storageBucket: "[YOUR_SECRET].appspot.com",
+  messagingSenderId: "[YOUR_SECRET]",
+  appId: "[YOUR_SECRET]",
+};
